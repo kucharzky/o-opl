270bc0f3fa80d96688557d1e319c4edd6d516c20
Maciej Kucharski. <overmax1407@gmail.com>
2024-11-21 12:20:36 +0100

    zadanie dom 1

---
+++ petle.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+int main(){
+std::vector< double > vec( 123 );
+    for( auto k { 0 }; k < vec.size(); ++ k )
+    {
+    vec[ k ] = 2 * k + 1;
+    }
+    auto k{0};
+    while(k<vec.size()){
+        vec[k] = 2*k+1;
+        k++;
+    }
+    
+    do{
+        vec[k] = 2*k+1;
+        k++;
+    }while(k<vec.size());
+}
